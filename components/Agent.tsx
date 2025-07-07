1cae72e944318132907c8d2187928fba5c643518
romeoaditya <romeoaditya22@gmail.com>
2025-07-07 18:16:12 +0700

    create feedback and show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {interviewer} from "@/constants";
+import {createFeedback} from "@/lib/actions/general.action";
 import {cn} from "@/lib/utils";
 import {vapi} from "@/lib/vapi.sdk";
 import Image from "next/image";
@@ -68,11 +69,11 @@ const Agent = ({
   const handleGenerateFeedback = async (messages: SavedMessage[]) => {
     console.log("Generate feedback here.");
 
-    // TODO: Create a server action that generates feedback
-    const {success, id} = {
-      success: true,
-      id: "feedback-id",
-    };
+    const {success, feedbackId: id} = await createFeedback({
+      interviewId: interviewId!,
+      userId: userId!,
+      transcript: messages,
+    });
     if (success && id) {
       router.push(`/interview/${interviewId}/feedback`);
     } else {
